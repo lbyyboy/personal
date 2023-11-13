c2f70aa20ba30304d871859392d6749181dc33af
lbyyboy <138211993+lbyyboy@users.noreply.github.com>
2023-11-13 14:11:15 +0900

    Add files via upload

--- js/jquery.js
+++ js/jquery.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
 
     //nav - submenu
 
-    $("nav ul li").mouseover(function(){
-        $(this).children(".submenu").stop().slideDown()
-    });
-    $("nav ul li").mouseleave(function(){
-        $(this).children(".submenu").stop().slideUp()
-    });
+    //$("nav ul li").mouseover(function(){
+        //$(this).children(".submenu").stop().slideDown()
+    //});
+    //$("nav ul li").mouseleave(function(){
+        //$(this).children(".submenu").stop().slideUp()
+    //});
 
 });
